ba72af54bf4b013790cddd62c1fe0a400ed97877
Trijavico <javier.penaperez08@gmail.com>
2025-01-13 11:44:53 -0400

    change Origin acces header from CORS middleware for frontend url

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -63,7 +63,7 @@ func Authenticated(next http.Handler) http.Handler {
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://notes-assesment.netlify.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
